cdc5c260cb893d6c047e16656ff968a2811f9b63
Denis Sergeev <zeff@altlinux.org>
2024-12-24 12:19:33 +0300

    docs: remove unused imports from core module

--- jsontool/core/__init__.py
+++ jsontool/core/__init__.py
@@ -7,8 +7,6 @@ JSON data.
 
 from .reader_writer import read_json_from_string, read_json_from_file, write_json_to_string, write_json_to_file
 from .modifier import add_key_to_json, add_element_to_json_array
-from .navigator import navigate_json
-from .validator import validate_json
 
 __all__ = [
     "read_json_from_string",
@@ -17,6 +15,4 @@ __all__ = [
     "write_json_to_file",
     "add_key_to_json",
     "add_element_to_json_array",
-    "navigate_json",
-    "validate_json",
 ]
